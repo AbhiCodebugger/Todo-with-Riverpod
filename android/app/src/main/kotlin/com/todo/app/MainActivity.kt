151a3743da9cb57fba0e927c6a2d8961bbd25fe4
Abhinav Ranjan <ranjansme12@gmail.com>
2023-12-29 14:52:48 +0530

    latest commit

--- android/app/src/main/kotlin/com/todo/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/todo/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.my_todo_app
+package com.todo.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_todo_app/firebase_options.dart';
 import 'package:my_todo_app/view/todo_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
